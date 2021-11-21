cc45cb95b4515648321e4c7e1087714ca05f43cf
daniel-herencia <daniherencia@gmail.com>
2021-11-21 14:55:40 +0100

    Commit comms2

--- board2/Core/Inc/comms.h
+++ board2/Core/Inc/comms.h
@@ -1,33 +1,21 @@
 /*
  * comms.h
  *
- *  Created on: Apr 20, 2021
- *      Author: guifre
+ *  Created on: Nov 17, 2021
+ *      Author: Daniel Herencia Ruiz
  */
 
 #ifndef INC_COMMS_H_
 #define INC_COMMS_H_
 
-//#include "main.h"
-#include "definitions.h"
+void configuration(void);
 
-/*Telecommand received from comms
-unpacking() -> redirect to the function we need to execute*/
-void telecommand(void);
+void tx_function(void);
 
-/*send the housekeeping (system_state) to the GS*/
-void sendtelemetry();
+void rx_function(void);
 
-/*Includes the packing of the data and telemetry(SPG4) we want to transmit to GS*/
-void transmitpck();
+void packaging(void);
 
-/*Includes the unpacking and executes the telecomands received from GS*/
-void receivepck(void);
 
-/*The size of each packet is 59Bytes (51Bytes of data)*/
-/*Each packet must contain the following fields: ([telecomand],[size of data], [data])*/
-void packing();
-void unpacking();
 
 #endif /* INC_COMMS_H_ */
-
--- board2/Core/Src/comms.c
+++ board2/Core/Src/comms.c
@@ -1,47 +1,33 @@
-/*
- * comms.cpp
- *
- *  Created on: Apr 20, 2021
- *      Author: guifre
- */
-
 #include "comms.h"
+//#include "sx126x.h"
+//#include "radio.h"
+//#include "sx126x-hal.h"
 
-void telecommand(void){
-//Hem de definir que volem fer aquí. //Enviar telecomands a la OBC??
-	//todo pensar com tractarem cada cas/parlar amb payload/comms
+void configuration(void){
+	//Inicialize power in the antenna and the transceiver => OBC??????????????
+	if( GetOperatingMode( ) != STDBY_RC )
+	{
+		SetStanby(STDBY_RC);
+	}
 
 
 
-}
-void sendtelemetry(){
-//Enviar un paquet de telemetria
-	/* Hem de pensar amb una estructura:
-	 *Agafem telemetria
-	 *La segmentem
-	 *Enviem
-	 */
+
+
 };
 
-void transmitpck(){
-	/*Agafem un paquet
-	 * l'enviem
-	 */
-}
+void tx_function(void){
 
 
-void receivepck(){
-	/*Agafem el paquet rebut
-	 * El desegmentem
-	 * Llegim instrucció
-	 */
-}
+};
 
-void packing(){
-	//Seria l'equivalent a segmentejar usat en Tx
-}
+void rx_function(void){
 
-void unpacking(){
-	//Seria l'invers a segmentejar usat en Rx
-}
+
+};
+
+void packaging(void){
+
+
+};
 
